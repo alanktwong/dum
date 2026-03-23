8bd884f3482ee5805c1ef1fb116a3a8b5974d1ea
Alan Wong <alan.wong@aya.yale.com>
2026-03-23 16:45:27 -0400

    feat: add bash to TaskType enum

--- pkg/enums/task_type.go
+++ pkg/enums/task_type.go
@@ -2,7 +2,7 @@
 package enums
 
 // TaskType is an enum source.
-// ENUM(cask, cellar, brew, dir, function,  git, link, jetbrains, mas, vscode).
+// ENUM(cask, cellar, brew, dir, function,  git, link, jetbrains, mas, vscode, bash).
 //
 //go:generate ../../bin/go-enum --ptr --marshal --flag --nocase --mustparse --names --values --nocomments
 type TaskType string
