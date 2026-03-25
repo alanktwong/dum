99f0406b994dab0a5eb4dc4d0a33d1f369422d05
Alan Wong <alan.wong@aya.yale.com>
2026-03-25 11:41:07 -0400

    refactor(plays): move PlayBookInfo to gen/ subpackage

--- pkg/plays/play_input.go
+++ pkg/plays/play_input.go
@@ -1,10 +1,12 @@
 // Package plays provides Play types and related utilities.
 package plays
 
+import "awong/dotfiles/pkg/plays/gen"
+
 // PlayInput holds the input parameters for executing a play.
 type PlayInput struct {
 	DryRun   bool
 	Play     string
-	PlayBook PlayBookInfo
+	PlayBook gen.PlayBookInfo
 	Sudo     bool
 }
