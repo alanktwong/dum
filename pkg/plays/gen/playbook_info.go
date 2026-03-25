cca446ad679d06fd3fc28102a81bc4273fcc78c6
Alan Wong <alan.wong@aya.yale.com>
2026-03-25 11:41:07 -0400

    refactor(plays): move PlayBookInfo to gen/ subpackage

--- pkg/plays/gen/playbook_info.go
+++ pkg/plays/gen/playbook_info.go
@@ -1,5 +1,5 @@
-// Package plays provides Play types and related utilities.
-package plays
+// Package gen provides generated types for plays package.
+package gen
 
 // PlayBookInfo provides information about a playbook.
 type PlayBookInfo interface {
